2f895f84ecdaa98a9ad6229df2b7889e2d096ca4
Akashns007 <mrpeculiar007@gmail.com>
2024-06-25 19:34:34 +0530

    encapsulation second commit

--- corejava/src/com/tnsif/encapsulation/Encapsulation.java
+++ corejava/src/com/tnsif/encapsulation/Encapsulation.java
@@ -2,11 +2,39 @@
 package com.tnsif.encapsulation;
 
 public class Encapsulation {
-	int serialNum = 10; //Data variable
-	String name;
-	int age;
+	// if no access specifier then it is default
+	private int serialNum = 10; //Data variable
+	private String name;
+	private int age;
+	//use tostrings by right clicking and sources and generate tostring()
+	//by using getter and setters we can right click and click on sources and generate getters and setters
+	
 	
 	void show() {
 		System.out.println("serialNumber:"+serialNum+"\t Name:"+name + "\t Age:"+age);
+	}
+	
+	@Override
+	public String toString() {
+		return "Encapsulation [serialNum=" + serialNum + ", name=" + name + ", age=" + age + "]";
+	}
+
+	public int getSerialNum() {
+		return serialNum;
+	}
+	public void setSerialNum(int serialNum) {
+		this.serialNum = serialNum;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
 	}
 }
--- corejava/src/com/tnsif/encapsulation/EncapsulationDemo.java
+++ corejava/src/com/tnsif/encapsulation/EncapsulationDemo.java
@@ -3,9 +3,13 @@ package com.tnsif.encapsulation;
 public class EncapsulationDemo {
 	public static void main(String[] args) {
 		Encapsulation ob = new Encapsulation();
-		ob.name= "suma";
-		ob.age = 33;
-		ob.serialNum=22;
-		ob.show();
+//		ob.name= "suma";
+//		ob.age = 33;
+//		ob.serialNum=22;
+		
+		ob.setAge(22);
+		ob.setName("thorfinn");
+		ob.setSerialNum(55);
+		System.out.println(ob);
 	}
 }
