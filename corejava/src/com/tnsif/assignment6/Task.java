098c5fe31f2108ee0bf0ca73fb232368beb2fe48
Akashns007 <mrpeculiar007@gmail.com>
2024-07-22 20:13:05 +0530

    assignment 6

---
+++ corejava/src/com/tnsif/assignment6/Task.java
@@ -0,0 +1,5 @@
+package com.tnsif.assignment6;
+
+public interface Task {
+	public void execute();
+}
---
+++ corejava/src/com/tnsif/assignment6/TaskScheduler.java
@@ -0,0 +1,21 @@
+package com.tnsif.assignment6;
+
+public class TaskScheduler {
+
+	public void scheduleTask(Task task, int interval) {
+		new Thread(() -> {
+		      while (interval >= 0) {
+		        try {
+		          task.execute();
+		          Thread.sleep(interval * 1000); 
+		        } catch (Exception e) {
+		          System.out.println("Exception raised "+e.getMessage());
+		          break;
+		        }
+		      }
+		    }).start();
+		
+	}
+}
+
+	
